344e4333ebe110f39b7f0bca2341cb6515e98c54
brainfck <mykhailo.tsuber@gmail.com>
2024-07-04 12:46:47 +0200

    project 1 initial check-ing

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,12 @@
+//Created by brainfck on 7/4/24.
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
