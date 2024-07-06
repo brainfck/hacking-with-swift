bacae8336e0b7db8246c3301d72fb03648e4535a
brainfck <mykhailo.tsuber@gmail.com>
2024-07-06 09:34:41 +0200

    challenge 1

---
+++ TempConverter/TempConverter/TempConverterApp.swift
@@ -0,0 +1,12 @@
+// Created by brainfck on 7/6/24.
+
+import SwiftUI
+
+@main
+struct TempConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
