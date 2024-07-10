4855b5d402c1b0fe721825a9f84121fefb890943
brainfck <mykhailo.tsuber@gmail.com>
2024-07-10 22:24:54 +0200

    project 3 initial

---
+++ project3/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,12 @@
+// Created by brainfck on 7/10/24.
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
