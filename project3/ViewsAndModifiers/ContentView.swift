0c724c765983ad93eb1dd516ace1627e0cfbf741
brainfck <mykhailo.tsuber@gmail.com>
2024-07-11 17:34:04 +0200

    day 24 challenge

--- project3/ViewsAndModifiers/ContentView.swift
+++ project3/ViewsAndModifiers/ContentView.swift
@@ -9,10 +9,26 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+              .titleStyle()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(.red)
     }
+}
+
+struct Title: ViewModifier {
+  func body(content: Content) -> some View {
+    content
+      .font(.largeTitle)
+      .foregroundStyle(.blue)
+      .padding()
+  }
+}
+
+extension View {
+  func titleStyle() -> some View {
+    modifier(Title())
+  }
 }
 
 #Preview {
