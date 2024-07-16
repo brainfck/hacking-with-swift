ce80107abc7f19f2a7ed8b9a6326849939dc2c3b
brainfck <mykhailo.tsuber@gmail.com>
2024-07-16 20:39:28 +0200

    challenge 2 initial

---
+++ challenge2/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,12 @@
+// Created by brainfck on 7/16/24.
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
