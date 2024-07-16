56900692175371a295e58401119381b7ab8da4d3
brainfck <mykhailo.tsuber@gmail.com>
2024-07-16 21:32:25 +0200

    challenge day 25

--- challenge2/RockPaperScissors/ContentView.swift
+++ challenge2/RockPaperScissors/ContentView.swift
@@ -3,17 +3,113 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  
+  let moves = ["🗿", "💵", "✂️"]
+  let winningMoves = ["💵", "✂️", "🗿"]
+  let loosingMoves = ["✂️", "🗿", "💵"]
+  
+  let maxMoves = 10
+  
+  @State private var winGoal = Bool.random()
+  @State private var currentMove = Int.random(in: 0...2)
+  @State private var userScore = 0
+  @State private var movesCounter = 0
+  @State private var showingScore = false
+  @State private var showingFinal = false
+  @State private var scoreTitle = ""
+  
+  var correctMove: String {
+    if winGoal {
+      return winningMoves[currentMove]
+    } else {
+      return loosingMoves[currentMove]
     }
+  }
+  
+  var body: some View {
+    ZStack {
+      RadialGradient(stops: [
+        .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+        .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+      ], center: .top, startRadius: 200, endRadius: 400)
+      .ignoresSafeArea()
+      
+      VStack {
+        Text(userScore >= movesCounter ? "😭" : "")
+        
+        HStack {
+          Text(winGoal ? "Win" : "Loose")
+            .fontWeight(.bold)
+          Text("vs")
+          MoveView(text: moves[currentMove])
+        }
+        
+        HStack {
+          ForEach(moves, id: \.self) { move in
+            Button {
+              moveTapped(move)
+            } label: {
+              MoveView(text: move)
+            }
+          }
+        }
+        Text("Score: \(userScore) of \(movesCounter)")
+        
+      }
+      .padding()
+    }
+    .alert(scoreTitle, isPresented: $showingScore) {
+      Button("I want more", action: makeMove)
+    } message: {
+      Text("Your score is \(userScore)")
+    }
+    .alert("Game Over", isPresented: $showingFinal) {
+      Button("Reset", action: resetGame)
+    } message: {
+      Text("Game over. You scored")
+    }
+  }
+  
+  func moveTapped(_ move: String) {
+    movesCounter += 1
+    
+    if move == correctMove {
+      userScore += 1
+      scoreTitle = "Winner"
+    } else {
+      scoreTitle = "LOOSER"
+    }
+    
+    if movesCounter >= maxMoves {
+      showingFinal = true
+    } else {
+      showingScore = true
+    }
+  }
+  
+  func makeMove() {
+    winGoal = Bool.random()
+  }
+  
+  func resetGame() {
+    userScore = 0
+    movesCounter = 0
+    showingFinal = false
+    makeMove()
+  }
+}
+
+struct MoveView: View {
+  var text: String
+  
+  var body: some View {
+    Text(text)
+      .font(.largeTitle)
+      .shadow(radius: 30)
+      .padding()
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
