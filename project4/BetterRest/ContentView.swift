7f9434934a8789e4c9805585a80bc64ff4273cfd
brainfck <mykhailo.tsuber@gmail.com>
2024-07-17 21:36:55 +0200

    project 4 initial files

---
+++ project4/BetterRest/ContentView.swift
@@ -0,0 +1,19 @@
+// Created by brainfck on 7/17/24.
+
+import SwiftUI
+
+struct ContentView: View {
+  
+  @State private var sleepAmount = 8.0
+  
+  var body: some View {
+    VStack {
+      Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
