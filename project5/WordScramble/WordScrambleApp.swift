42aa8faec2e7f98838b53d2935b504decf6a9d07
brainfck <mykhailo.tsuber@gmail.com>
2024-07-22 12:15:17 +0200

    project 5 initial setup

---
+++ project5/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,12 @@
+// Created by brainfck on 7/22/24.
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
