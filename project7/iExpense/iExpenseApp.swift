0d2dac30c960689a7f2a11c98b01faf3996eb7fe
brainfck <mykhailo.tsuber@gmail.com>
2024-07-29 11:59:37 +0200

    project 7 initial setup

---
+++ project7/iExpense/iExpenseApp.swift
@@ -0,0 +1,12 @@
+// Created by brainfck on 7/29/24.
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
