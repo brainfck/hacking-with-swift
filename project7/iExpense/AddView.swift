373a94b4c1c4c8c3c7f58e67653e5bd91d0d5d71
brainfck <mykhailo.tsuber@gmail.com>
2024-07-30 22:10:28 +0200

    Sharing an observed object with a new view

---
+++ project7/iExpense/AddView.swift
@@ -0,0 +1,35 @@
+// Created by brainfck on 7/30/24.
+
+import SwiftUI
+
+struct AddView: View {
+  @State private var name = ""
+  @State private var type = "Personal"
+  @State private var amount = 0.0
+  
+  var expenses: Expenses
+  
+  let types = ["Business", "Personal"]
+  
+  var body: some View {
+    NavigationStack {
+      Form {
+        TextField("Name", text: $name)
+        
+        Picker("Type", selection: $type) {
+          ForEach(types, id: \.self) {
+            Text($0)
+          }
+        }
+        
+        TextField("Amount", value: $amount, format: .currency(code: "USD"))
+          .keyboardType(.decimalPad)
+      }
+      .navigationTitle("Add new expense")
+    }
+  }
+}
+
+#Preview {
+  AddView(expenses: Expenses())
+}
--- project7/iExpense/ContentView.swift
+++ project7/iExpense/ContentView.swift
@@ -5,7 +5,7 @@ import Observation
 
 struct ContentView: View {
   @State private var expenses = Expenses()
-  
+  @State private var showingAddExpense = false
   
   var body: some View {
     NavigationStack {
@@ -18,9 +18,11 @@ struct ContentView: View {
       .navigationTitle("iExpense")
       .toolbar {
         Button("Add Expense", systemImage: "plus") {
-          let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
-          expenses.items.append(expense)
+          showingAddExpense = true
         }
+      }
+      .sheet(isPresented: $showingAddExpense) {
+        AddView(expenses: expenses)
       }
     }
   }
