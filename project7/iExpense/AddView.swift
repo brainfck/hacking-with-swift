6215a8d3d6afd957d206d97cca481bac63ca0c9a
brainfck <mykhailo.tsuber@gmail.com>
2024-07-30 22:25:14 +0200

    Day37. Final polish

--- project7/iExpense/AddView.swift
+++ project7/iExpense/AddView.swift
@@ -7,6 +7,8 @@ struct AddView: View {
   @State private var type = "Personal"
   @State private var amount = 0.0
   
+  @Environment(\.dismiss) var dismiss
+  
   var expenses: Expenses
   
   let types = ["Business", "Personal"]
@@ -26,6 +28,13 @@ struct AddView: View {
           .keyboardType(.decimalPad)
       }
       .navigationTitle("Add new expense")
+      .toolbar {
+        Button("Save") {
+          let item = ExpenseItem(name: name, type: type, amount: amount)
+          expenses.items.append(item)
+          dismiss()
+        }
+      }
     }
   }
 }
