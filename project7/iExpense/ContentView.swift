28fca72031bea21fe1f884fbbf375ee73d1a57df
brainfck <mykhailo.tsuber@gmail.com>
2024-07-31 16:01:07 +0200

    day38 challenge

--- project7/iExpense/ContentView.swift
+++ project7/iExpense/ContentView.swift
@@ -9,35 +9,61 @@ struct ContentView: View {
   
   var body: some View {
     NavigationStack {
-      List {
-        ForEach(expenses.items) { item in
-          HStack {
-            VStack(alignment: .leading) {
-              Text(item.name)
-                .font(.headline)
-              Text(item.type)
+      Section("Personal") {
+        List {
+          ForEach(expenses.items) { item in
+            if(item.type == "Personal") {
+              ExpenseView(name: item.name, type: item.type, amount: item.amount)
             }
-            
-            Spacer()
-            Text(item.amount, format: .currency(code: "USD"))
+          }
+          .onDelete(perform: removeItems)
+        }
+        .navigationTitle("iExpense")
+        .toolbar {
+          Button("Add Expense", systemImage: "plus") {
+            showingAddExpense = true
           }
         }
-        .onDelete(perform: removeItems)
       }
-      .navigationTitle("iExpense")
-      .toolbar {
-        Button("Add Expense", systemImage: "plus") {
-          showingAddExpense = true
+      
+      Section("Business") {
+        List {
+          ForEach(expenses.items) { item in
+            if(item.type == "Business") {
+              ExpenseView(name: item.name, type: item.type, amount: item.amount)
+            }
+          }
+          .onDelete(perform: removeItems)
         }
       }
-      .sheet(isPresented: $showingAddExpense) {
-        AddView(expenses: expenses)
-      }
+    }
+    .sheet(isPresented: $showingAddExpense) {
+      AddView(expenses: expenses)
     }
   }
   
   func removeItems(at offsets: IndexSet) {
     expenses.items.remove(atOffsets: offsets)
+  }
+}
+
+struct ExpenseView: View {
+  let name: String
+  let type: String
+  let amount: Double
+  
+  var body: some View {
+    HStack {
+      VStack(alignment: .leading) {
+        Text(name)
+          .font(.headline)
+        Text(type)
+      }
+      
+      Spacer()
+      Text(amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+        .foregroundStyle(amount < 10 ? .green : amount < 100 ? .blue : .red)
+    }
   }
 }
 
