108a3a201cdcd1349782b1320dcd78d1e2f3e795
brainfck <mykhailo.tsuber@gmail.com>
2024-08-02 10:24:02 +0200

    project 8 initial setup

---
+++ project8/Moonshot/MoonshotApp.swift
@@ -0,0 +1,12 @@
+// Created by brainfck on 8/2/24.
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
