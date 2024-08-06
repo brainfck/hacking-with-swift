52bb40635c905d955facfc1291c5906e677a5e6d
brainfck <mykhailo.tsuber@gmail.com>
2024-08-06 22:46:39 +0200

    Finishing up with one last view

---
+++ project8/Moonshot/AstronautView.swift
@@ -0,0 +1,30 @@
+// Created by brainfck on 8/6/24.
+
+import SwiftUI
+
+struct AstronautView: View {
+  let astronaut: Astronaut
+  
+  var body: some View {
+    ScrollView {
+      VStack {
+        Image(astronaut.id)
+          .resizable()
+          .scaledToFit()
+        
+        Text(astronaut.description)
+          .padding()
+      }
+      .background(.darkBackground)
+      .navigationTitle(astronaut.name)
+      .navigationBarTitleDisplayMode(.inline)
+    }
+  }
+}
+
+#Preview {
+  let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+  
+  return AstronautView(astronaut: astronauts["aldrin"]!)
+    .preferredColorScheme(.dark)
+}
