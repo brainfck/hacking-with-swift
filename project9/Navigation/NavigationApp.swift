f61d20e32778386455a64fc444ae8044d5170d6b
brainfck <mykhailo.tsuber@gmail.com>
2024-08-27 21:42:03 +0200

    project 9 initial commit

---
+++ project9/Navigation/NavigationApp.swift
@@ -0,0 +1,12 @@
+// Created by brainfck on 8/27/24.
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
