1be4a7d95306ad15cda932f04be2bdc5cf6f4739
brainfck <mykhailo.tsuber@gmail.com>
2024-08-28 14:59:02 +0200

    Making your navigation title editable

--- project9/Navigation/ContentView.swift
+++ project9/Navigation/ContentView.swift
@@ -3,31 +3,13 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var title = "SwiftUI"
+  
   var body: some View {
     NavigationStack {
       Text("Hello, World!")
-      .toolbar {
-        ToolbarItemGroup(placement: .topBarLeading) {
-          Button("Tap me") {
-            
-          }
-          Button("Tap me 2") {
-            
-          }
-        }
-        
-        ToolbarItem(placement: .topBarLeading) {
-          Button("Tap me") {
-            
-          }
-        }
-        
-        ToolbarItem(placement: .topBarLeading) {
-          Button("Or Tap me") {
-            
-          }
-        }
-      }
+      .navigationTitle($title)
+      .navigationBarTitleDisplayMode(.inline)
     }
   }
 }
