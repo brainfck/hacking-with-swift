0e86c9fb52eee6eb5e0a5046b64320b63f346177
brainfck <mykhailo.tsuber@gmail.com>
2024-09-02 12:21:42 +0200

    Validating and disabling forms

--- project10/CupcakeCorner/ContentView.swift
+++ project10/CupcakeCorner/ContentView.swift
@@ -3,71 +3,30 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var results = [Result]()
+  @State private var username = ""
+  @State private var email = ""
+  
+  var disabledForm: Bool {
+    username.count < 5 || email.count < 5
+  }
   
   var body: some View {
-    AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
-      image
-        .resizable()
-        .scaledToFit()
-    } placeholder: {
-      ProgressView()
-    }
-    .frame(width: 200, height: 200)
-    
-    AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
-      if let image = phase.image {
-        image
-          .resizable()
-          .scaledToFit()
-      } else if phase.error != nil {
-        Text("There was an error loading the image.")
-      } else {
-        ProgressView()
-      }
-    }
-    .frame(width: 200, height: 200)
-    
-    List(results, id: \.trackId) { item in
-      VStack(alignment: .leading) {
-        Text(item.trackName)
-          .font(.headline)
-        Text(item.collectionName)
-      }
-    }
-    .task {
-      await loadData()
-    }
-  }
-  
-  func loadData() async {
-    guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-      print("Invalid URL")
-      return
-    }
-    
-    do {
-      let(data, _) = try await URLSession.shared.data(from: url)
-      
-      if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-        results = decodedResponse.results
+    Form {
+      Section {
+        TextField("Username", text: $username)
+        TextField("Email", text: $email)
       }
       
-    } catch {
-      print("Invalid data")
+      Section {
+        Button("Create account") {
+          print("Creating account")
+        }
+      }
+      .disabled(disabledForm)
     }
   }
 }
 
-struct Response: Codable {
-  var results: [Result]
-}
-
-struct Result: Codable {
-  var trackId: Int
-  var trackName: String
-  var collectionName: String
-}
 
 #Preview {
   ContentView()
