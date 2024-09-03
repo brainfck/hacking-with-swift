7ca8a4a56995cee785c2735d5a42432e30029968
brainfck <mykhailo.tsuber@gmail.com>
2024-09-03 23:26:32 +0200

    Taking basic order details

---
+++ project10/CupcakeCorner/AddressView.swift
@@ -0,0 +1,15 @@
+// Created by brainfck on 9/3/24.
+
+import SwiftUI
+
+struct AddressView: View {
+  var order: Order
+  
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+#Preview {
+  AddressView(order: Order())
+}
---
+++ project10/CupcakeCorner/Order.swift
@@ -0,0 +1,22 @@
+// Created by brainfck on 9/3/24.
+
+import SwiftUI
+
+@Observable
+class Order {
+  static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+  
+  var type = 0
+  var quantity = 3
+  
+  var specialRequestEnabled = false {
+    didSet {
+      if specialRequestEnabled == false {
+        extraFrosting = false
+        addSprinkles = false
+      }
+    }
+  }
+  var extraFrosting = false
+  var addSprinkles = false
+}
