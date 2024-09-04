049ded6ea917b6a9cbad38df7a337bf0bdd46852
brainfck <mykhailo.tsuber@gmail.com>
2024-09-04 22:11:09 +0200

    Checking for a valid address

--- project10/CupcakeCorner/Order.swift
+++ project10/CupcakeCorner/Order.swift
@@ -9,6 +9,11 @@ class Order {
   var type = 0
   var quantity = 3
   
+  var name = ""
+  var streetAddress = ""
+  var city = ""
+  var zip = ""
+  
   var specialRequestEnabled = false {
     didSet {
       if specialRequestEnabled == false {
@@ -19,4 +24,12 @@ class Order {
   }
   var extraFrosting = false
   var addSprinkles = false
+  
+  var hasValidAddress: Bool {
+    if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+      return false
+    }
+    
+    return true
+  }
 }
