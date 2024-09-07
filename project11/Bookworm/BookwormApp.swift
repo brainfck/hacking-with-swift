d871736f66abe71c3a0842c3e7c3d46daa701f7e
brainfck <mykhailo.tsuber@gmail.com>
2024-09-07 15:47:04 +0200

    project 11 initial commit

---
+++ project11/Bookworm/BookwormApp.swift
@@ -0,0 +1,12 @@
+// Created by brainfck on 9/7/24.
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
