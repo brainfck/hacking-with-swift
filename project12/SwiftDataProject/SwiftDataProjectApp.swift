7121518819249ccbdbe5e704a7b366ba11d5617e
brainfck <mykhailo.tsuber@gmail.com>
2024-09-10 14:50:42 +0200

    project12 initial files

---
+++ project12/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,12 @@
+// Created by brainfck on 9/10/24.
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
