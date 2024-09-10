f195b0e6e9630b5c52a0626a4460482a5d117b94
brainfck <mykhailo.tsuber@gmail.com>
2024-09-10 15:41:38 +0200

    Filtering @Query using Predicate

--- project12/SwiftDataProject/ContentView.swift
+++ project12/SwiftDataProject/ContentView.swift
@@ -5,7 +5,19 @@ import SwiftUI
 
 struct ContentView: View {
   @Environment(\.modelContext) var modelContext
-  @Query(sort: \User.name) var users: [User]
+  
+  @Query(filter: #Predicate<User> { user in
+    if user.name.localizedStandardContains("R") {
+      if user.city == "London" {
+        return true
+      } else {
+        return false
+      }
+    } else {
+      return false
+    }
+  }, sort: \User.name) var users: [User]
+  
   @State private var path = [User]()
 
   var body: some View {
@@ -20,10 +32,18 @@ struct ContentView: View {
         EditUserView(user: user)
       }
       .toolbar {
-        Button("Add User", systemImage: "plus") {
-          let user = User(name: "", city: "", joinDate: .now)
-          modelContext.insert(user)
-          path = [user]
+        Button("Add Samples", systemImage: "plus") {
+          try? modelContext.delete(model: User.self)
+          
+          let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
+          let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
+          let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
+          let fourth = User(name: "Johnny English", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
+          
+          modelContext.insert(first)
+          modelContext.insert(second)
+          modelContext.insert(third)
+          modelContext.insert(fourth)
         }
       }
     }
