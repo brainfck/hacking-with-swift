d1ba8704906acc895c4ee6871ab73eb892c9a1a2
brainfck <mykhailo.tsuber@gmail.com>
2024-09-12 15:56:47 +0200

    initial commit

---
+++ challenge3/HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,12 @@
+// Created by brainfck on 9/12/24.
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ challenge4/MyFriends/MyFriendsApp.swift
@@ -0,0 +1,12 @@
+// Created by brainfck on 9/12/24.
+
+import SwiftUI
+
+@main
+struct MyFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
