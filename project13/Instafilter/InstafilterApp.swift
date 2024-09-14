d99c5c2e20716b9d69168e1539b88cf18b44bfc7
brainfck <mykhailo.tsuber@gmail.com>
2024-09-14 12:06:45 +0200

    initial commit

---
+++ project13/Instafilter/InstafilterApp.swift
@@ -0,0 +1,12 @@
+// Created by brainfck on 9/14/24.
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
