7853643db04fa6168f5ca353775a260a9412b4a6
brainfck <mykhailo.tsuber@gmail.com>
2024-09-18 21:44:36 +0200

    Switching view states with enums

--- project14/BucketList/ContentView.swift
+++ project14/BucketList/ContentView.swift
@@ -8,21 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var loadingState = LoadingState.loading
+  
+  enum LoadingState {
+    case loading, success, failed
+  }
+  
   var body: some View {
-    Button("Read and Write") {
-      let data = Data("Test Message".utf8)
-      let url = URL.documentsDirectory.appending(path: "message.txt")
-      
-      do {
-        try data.write(to: url, options: [.atomic, .completeFileProtection])
-        let input = try String(contentsOf: url, encoding: .utf8)
-        print(input)
-      } catch {
-        print(error.localizedDescription)
-      }
+    switch loadingState {
+    case .loading:
+      LoadingView()
+    case .success:
+      SuccessView()
+    case .failed:
+      FailedView()
     }
   }
 }
+
+struct LoadingView: View {
+  var body: some View {
+    Text("Loading...")
+  }
+}
+
+struct SuccessView: View {
+  var body: some View {
+    Text("Success!")
+  }
+}
+
+struct FailedView: View {
+  var body: some View {
+    Text("Failed.")
+  }
+}
+
+//struct ContentView: View {
+//  var body: some View {
+//    Button("Read and Write") {
+//      let data = Data("Test Message".utf8)
+//      let url = URL.documentsDirectory.appending(path: "message.txt")
+//      
+//      do {
+//        try data.write(to: url, options: [.atomic, .completeFileProtection])
+//        let input = try String(contentsOf: url, encoding: .utf8)
+//        print(input)
+//      } catch {
+//        print(error.localizedDescription)
+//      }
+//    }
+//  }
+//}
 
 //struct ContentView: View {
 //  let users: [User] = [
