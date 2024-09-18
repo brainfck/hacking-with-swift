4810807d3d78d28d5db43a906ca1d303f4579c28
brainfck <mykhailo.tsuber@gmail.com>
2024-09-18 21:25:25 +0200

    project 14 initial commit

---
+++ project14/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Mykhailo Tsuber on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
