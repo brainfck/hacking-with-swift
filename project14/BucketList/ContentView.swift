24f5bc2526305ef39e0aa0e94c712ee15ca01547
brainfck <mykhailo.tsuber@gmail.com>
2024-09-18 21:40:32 +0200

    Writing data to the documents directory

--- project14/BucketList/ContentView.swift
+++ project14/BucketList/ContentView.swift
@@ -8,28 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-  let users: [User] = [
-    .init(firstName: "Arnorld", lastName: "Rimmer"),
-    .init(firstName: "Kristine", lastName: "Kochanski"),
-    .init(firstName: "David", lastName: "Lister"),
-  ].sorted()
-  
   var body: some View {
-    List(users) { user in
-      Text("\(user.lastName), \(user.firstName)")
+    Button("Read and Write") {
+      let data = Data("Test Message".utf8)
+      let url = URL.documentsDirectory.appending(path: "message.txt")
+      
+      do {
+        try data.write(to: url, options: [.atomic, .completeFileProtection])
+        let input = try String(contentsOf: url, encoding: .utf8)
+        print(input)
+      } catch {
+        print(error.localizedDescription)
+      }
     }
   }
 }
 
-struct User: Identifiable, Comparable {
-  let id = UUID()
-  var firstName: String
-  let lastName: String
-  
-  static func < (lhs: User, rhs: User) -> Bool {
-    lhs.lastName < rhs.lastName
-  }
-}
+//struct ContentView: View {
+//  let users: [User] = [
+//    .init(firstName: "Arnorld", lastName: "Rimmer"),
+//    .init(firstName: "Kristine", lastName: "Kochanski"),
+//    .init(firstName: "David", lastName: "Lister"),
+//  ].sorted()
+//  
+//  var body: some View {
+//    List(users) { user in
+//      Text("\(user.lastName), \(user.firstName)")
+//    }
+//  }
+//}
+//
+//struct User: Identifiable, Comparable {
+//  let id = UUID()
+//  var firstName: String
+//  let lastName: String
+//  
+//  static func < (lhs: User, rhs: User) -> Bool {
+//    lhs.lastName < rhs.lastName
+//  }
+//}
 
 #Preview {
   ContentView()
