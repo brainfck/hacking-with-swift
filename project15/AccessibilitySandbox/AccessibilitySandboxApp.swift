f906fd09b0dfbf349216d6d56db0be47c70886ac
brainfck <mykhailo.tsuber@gmail.com>
2024-09-26 20:06:12 +0200

    project 15 initial files

---
+++ project15/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Mykhailo Tsuber on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
